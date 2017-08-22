72ddc2ec4b2e187450d3f342a5e7576f24c44d3e
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:03:01 +0200

    oneplus3: Update display/render for O
    * Build the various display/render related HIDL HALs
    * Enable HWC2 & Gralloc1
    
    Change-Id: I61c34a531e3131209a08b5b4670e1f1f8fbb435d

--- device.mk
+++ device.mk
@@ -166,12 +166,14 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    copybit.msm8996 \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
     gralloc.msm8996 \
     hwcomposer.msm8996 \
-    memtrack.msm8996 \
-    liboverlay \
-    libtinyxml
+    memtrack.msm8996
 
 # Display calibration
 PRODUCT_PACKAGES += \
@@ -275,7 +277,8 @@ PRODUCT_PACKAGES += \
 
 # QMI
 PRODUCT_PACKAGES += \
-    libjson
+    libjson \
+    libtinyxml
 
 # Ramdisk
 PRODUCT_PACKAGES += \
@@ -292,6 +295,10 @@ PRODUCT_PACKAGES += \
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_op3
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
 
 # RIL
 PRODUCT_PACKAGES += \
