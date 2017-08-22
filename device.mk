7f1928524ffee224ddddf39ae181bdb82cf87cc5
mj1687 <alessandro.pasini12@gmail.com>
2017-08-22 12:52:48 +0200

    ElixiumOS bringup
    Signed-off-by: mj1687 <alessandro.pasini12@gmail.com>

--- device.mk
+++ device.mk
@@ -300,12 +300,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     sensors.msm8996
 
-# Weather providers
-PRODUCT_PACKAGES += \
-    OpenWeatherMapWeatherProvider \
-    YahooWeatherProvider \
-    WundergroundWeatherProvider
-
 # Wifi
 PRODUCT_PACKAGES += \
     ipacm \
