05a4128436c87010e0a9cbe7acb5af7aa49a8c4f
LuK1337 <priv.luk@gmail.com>
2017-09-02 18:41:27 +0200

    oneplus3: Add basic 2.0 -> 2.1 fingerprint HAL wrapper
    Change-Id: I36752485e39c93691261d9cf90f9455c7de2648e

--- device.mk
+++ device.mk
@@ -171,7 +171,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-impl
 
 # Display
 PRODUCT_PACKAGES += \
@@ -197,6 +197,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service \
+    fingerprint.msm8996 \
     OneplusPocketMode
 
 # For config.fs
